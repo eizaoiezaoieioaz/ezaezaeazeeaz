a2a01449c8df58a1f9ec275ffdf56950ec5e8eeb
eizaoiezaoieioaz <115187400+eizaoiezaoieioaz@users.noreply.github.com>
2022-10-06 22:14:27 +0200

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const createError = require('http-errors');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const helmet = require('helmet');
+
+const indexRouter = require('./routes/index');
+
+const errorHandler = require('./middleware/errorHandler');
+
+const app = express();
+
+app.use(helmet()); // https://expressjs.com/en/advanced/best-practice-security.html#use-helmet
+app.use(express.json());
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:443');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError.NotFound());
+});
+
+// pass any unhandled errors to the error handler
+app.use(errorHandler);
+
+module.exports = app;
