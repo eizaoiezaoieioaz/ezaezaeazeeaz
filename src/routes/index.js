7630a084a21c8da8ba1c9a3999eea992d0a691c3
eizaoiezaoieioaz <115187400+eizaoiezaoieioaz@users.noreply.github.com>
2022-10-06 22:48:00 +0200

    Delete src directory

--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const ProductController = require('../controllers/ProductController');
-
-const router = express.Router();
-
-router.get("/api/products/", ProductController.getAll);
-
-module.exports = router;
